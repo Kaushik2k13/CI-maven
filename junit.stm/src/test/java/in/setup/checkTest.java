59326a1e078acc0169f7de6bc518851e883ff7e5
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-12 19:49:41 +0100

    unittest

--- junit.stm/src/test/java/in/setup/checkTest.java
+++ junit.stm/src/test/java/in/setup/checkTest.java
@@ -1,24 +1,26 @@
 package in.setup;
 import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.*;
 
 public class checkTest {
 	
-	private static check area = new check();
+	private check area = new check();
 	@Test
 	public void testSq_area() {
 		assertEquals(16,area.sq_area(4));
 	}
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testC_area() {
-		assertEquals(78.5, area.c_area(5), 0);
+		assertEquals(78.5, area.c_area(5));
 	}
 	@Test
 	public void testRec_area() {
 		assertEquals(12, area.rec_area(3, 4));
 	}
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testtri_area() {
-		assertEquals(7.875, area.tri_area(3.5, 4.5),0);
+		assertEquals(7.875, area.tri_area(3.5, 4.5));
 	}
 	}
