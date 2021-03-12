9f5b7f7770c57efde3da623cff93a3fc13baf27a
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-12 11:10:18 +0100

    initial commit

---
+++ junit.stm/src/main/java/in/setup/check.java
@@ -0,0 +1,16 @@
+package in.setup;
+
+
+public class check {
+	public int sq_area(int a) {
+		return(a*a);
+	}
+	public double c_area(int r) {
+		double  ca =2*(3.14)*r;
+		return(ca);
+	}
+	public int rec_area(int w, int l ) {
+		return(w*l);
+	}
+	
+}
---
+++ junit.stm/src/test/java/in/setup/checkTest.java
@@ -0,0 +1,24 @@
+package in.setup;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class checkTest {
+
+	@Test
+	public void testSq_area() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testC_area() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRec_area() {
+		fail("Not yet implemented");
+	}
+
+}
