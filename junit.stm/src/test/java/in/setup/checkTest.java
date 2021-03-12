756650a417b939a5bc1717a7f2d3511d2ee42d14
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-12 18:24:16 +0100

    junit test

--- junit.stm/src/test/java/in/setup/checkTest.java
+++ junit.stm/src/test/java/in/setup/checkTest.java
@@ -1,7 +1,5 @@
 package in.setup;
-
-import static org.junit.Assert.assertEquals;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class checkTest {
@@ -13,14 +11,14 @@ public class checkTest {
 	}
 	@Test
 	public void testC_area() {
-		assertEquals(78.5,area.c_area(5),0);
+		assertEquals(78.5, area.c_area(5), 0);
 	}
 	@Test
 	public void testRec_area() {
-		assertEquals(12,area.rec_area(3,4));
+		assertEquals(12, area.rec_area(3, 4));
 	}
 	@Test
 	public void testtri_area() {
-		assertEquals(7.875,area.tri_area(3.5,4.5),0);
+		assertEquals(7.875, area.tri_area(3.5, 4.5),0);
 	}
 	}
