a5fb97fb09e743c0043938fe6508e04867bd3c44
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-14 17:30:28 +0100

    runner class

--- junit.stm/src/test/java/in/setup/testing.java
+++ junit.stm/src/test/java/in/setup/testing.java
@@ -1,5 +1,7 @@
 package in.setup;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -7,5 +9,5 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({checkTest.class, inttest.class})
 public class testing {
-
+	Result result = JUnitCore.runClasses(inttest.class);
 }
