75835d1905541e43a92501637dbbbafa987e2105
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-14 03:24:03 +0100

    commit for integration testing

---
+++ junit.stm/src/test/java/in/setup/checkintegration.java
@@ -0,0 +1,28 @@
+package in.setup;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class checkintegration { 
+	@Test
+	public void testSq_area() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testC_area() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRec_area() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTri_area() {
+		fail("Not yet implemented");
+	}
+
+}
