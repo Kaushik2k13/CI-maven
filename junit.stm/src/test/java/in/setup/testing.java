db0c5e95d098d93a197b39b8441b7ea23e190deb
Kaushik2k13 <kaushik2k13@gmail.com>
2021-03-14 05:01:13 +0100

    commit for suite

---
+++ junit.stm/src/test/java/in/setup/testing.java
@@ -0,0 +1,11 @@
+package in.setup;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({checkTest.class, inttest.class})
+public class testing {
+
+}
